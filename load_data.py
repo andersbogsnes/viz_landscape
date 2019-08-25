dc7bb38bd7afa04f4d61602d5078796e5701d2c2
Anders Bogsnes <andersbogsnes@gmail.com>
2019-08-25 23:25:55 +0200

    Done with intermediate

--- load_data.py
+++ load_data.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import numpy as np
 
-def load_data():
-    df = pd.read_csv('./data/crime.csv', encoding='latin-1')
 
-    df['OCCURRED_ON_DATE'] = pd.to_datetime(df.OCCURRED_ON_DATE)
-    df['DAY_OF_WEEK'] = pd.Categorical(df.DAY_OF_WEEK, 
-                                       categories=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], 
-                                       ordered=True)
-    df['SHOOTING'] = np.where(df.SHOOTING == 'Y', 1, 0)
+def load_data():
+    df = pd.read_csv("../data/crime.csv", encoding="latin-1")
+
+    df["OCCURRED_ON_DATE"] = pd.to_datetime(df.OCCURRED_ON_DATE)
+    df["DAY_OF_WEEK"] = pd.Categorical(
+        df.DAY_OF_WEEK,
+        categories=[
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ],
+        ordered=True,
+    )
+    df["SHOOTING"] = np.where(df.SHOOTING == "Y", 1, 0)
     return df
